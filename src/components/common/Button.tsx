f25963d72184cfcf600b9eb0d9f5e03bbbc51f70
jrary <jrary@naver.com>
2024-07-22 16:56:30 +0900

    Feat: 버튼, 모달창 (#2) - 버튼을 컴포넌트로 자름. - 모달창

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from "styled-components";
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+  return (
+    <Btn onClick={onClick}>
+      {text}
+    </Btn>
+  );
+};
+
+const Btn = styled.div`
+    background-color: #FEE500;
+    width: 115px;
+    height: 35px;
+    text: 20px;
+    border-radius: 10px;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 16px;
+`;
--- src/components/common/SearchBox.tsx
+++ src/components/common/SearchBox.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import * as S from "./SearchBoxStyle";
+import styled from "styled-components";
 
 interface SearchBoxProps {
     menuItems: string[];
@@ -25,34 +25,141 @@ export const SearchBox: React.FC<SearchBoxProps> = ({ menuItems, onSearch }) => 
     }
 
     return (
-        <S.SearchComp>
-            <S.DropdownContainer>
-                <S.DropdownButton onClick={toggleDropdown} isopen={isopen}>
-                    <S.ButtonText>{selectedItem}</S.ButtonText>
-                    <S.ButtonIcon>{isopen ? '▲' : '▼'}</S.ButtonIcon>
-                </S.DropdownButton>
+        <SearchComp>
+            <DropdownContainer>
+                <DropdownButton onClick={toggleDropdown} isopen={isopen}>
+                    <ButtonText>{selectedItem}</ButtonText>
+                    <ButtonIcon>{isopen ? '▲' : '▼'}</ButtonIcon>
+                </DropdownButton>
                 {isopen && (
-                    <S.DropdownList>
+                    <DropdownList>
                         {menuItems.map(item => (
-                            <S.DropdownItem 
+                            <DropdownItem 
                                 key={item} 
                                 onClick={() => handleSelect(item)}
                                 className={item === selectedItem ? 'selected' : ''}
                             >
                                 {item}
-                            </S.DropdownItem>
+                            </DropdownItem>
                         ))}
-                    </S.DropdownList>
+                    </DropdownList>
                 )}
-            </S.DropdownContainer>
-            <S.InputComp>
-                <S.Input 
+            </DropdownContainer>
+            <InputComp>
+                <Input 
                     type="text" 
                     value={searchText} 
                     onChange={(e) => setSearchText(e.target.value)}
                 />
-                <S.Button onClick={handleSearch} src="public/img/search.png" alt="Search"/>
-            </S.InputComp>
-        </S.SearchComp>
+                <Button onClick={handleSearch} src="public/img/search.png" alt="Search"/>
+            </InputComp>
+        </SearchComp>
     );
 }
+
+// Style
+const SearchComp = styled.div`
+    height: 30px;
+    width: 330px;
+    display: flex;
+`;
+
+const DropdownContainer = styled.div`
+    height: 100%;
+    width: 30%;
+    margin-right: 5%;
+    position: relative;
+    display: flex;
+    align-items: center;
+`;
+
+const DropdownButton = styled.button<{ isopen: boolean }>`
+    height: 100%;
+    width: 100%;
+    border-radius: ${(props) => (props.isopen ? '10px 10px 0 0' : '10px')};
+    background-color: white;
+    border: 1px solid #686868;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    padding: 0 8px;
+`;
+
+const ButtonText = styled.span`
+    flex: 1;
+    text-align: center;
+`;
+
+const ButtonIcon = styled.span`
+    margin-left: 8px;
+`;
+
+const DropdownList = styled.ul`
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 98%;
+    margin: 0px;
+    padding: 0px;
+    list-style: none;
+    border: 1px solid #686868;
+    border-top: none;
+    border-radius: 0 0 10px 10px;
+    background-color: white;
+    z-index: 1000;
+    max-height: 150px;
+    overflow-y: auto;
+
+    display: flex;
+    flex-direction: column;
+`;
+
+const DropdownItem = styled.li`
+    height: 25px;
+    width: 100%;
+    padding: 0px;
+    cursor: pointer;
+    border-bottom: 1px solid #eee;
+
+    &:last-child {
+        border-bottom: none;
+    }
+
+    &:hover {
+        background-color: #eeeeee;
+    }
+
+    &.selected {
+        background-color: #cccccc;
+        color: white;
+    }
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const InputComp = styled.div`
+    height: 93%;
+    width: 65%;
+    border: 1px solid #686868;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+`;
+
+const Input = styled.input`
+    padding: 4px;
+    border: none;
+    border-radius: 10px;
+    outline: none;
+    flex: 1;
+`;
+
+const Button = styled.img`
+    height: 17px;
+    width: 17px;
+    padding: 4px 8px;
+    border: none;
+    cursor: pointer;
+`;
