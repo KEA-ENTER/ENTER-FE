9602f5409e524065f7777c1bb40eb14f17db9e8c
jrary <jrary@naver.com>
2024-07-31 15:25:00 +0900

    Feat: 인수/반납 보고서 페널티 관리, 모달창 연결 (#12)

--- src/components/admin/step/report/penalty/PenaltyManage.tsx
+++ src/components/admin/step/report/penalty/PenaltyManage.tsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import styled from "styled-components";
 import PenaltyMenu from './PenaltyMenu';
 import Button from '../../../basic/Button';
+import Modal from '../../../basic/Modal';
 
 const PenaltyManage: React.FC = () => {
     const menuItems = ['메롱메롱', '너는 바보', '하기싫다', '살려줘'];
     const levelItems = ['개작은페널티', '괜찮', '개큰페널티'];
     const [penalties, setPenalties] = useState<{ reason: string, remark: string }[]>([{ reason: menuItems[0], remark: '' }]);
+    const [alertModal, setAlertModal] = useState(false);
+    const [confirmModal, setConfirmModal] = useState(false);
     const [openMenuIndex, setOpenMenuIndex] = useState<number | null>(null);
     const [openLevelIndex, setOpenLevelIndex] = useState<number | null>(null);
 
@@ -20,7 +23,24 @@ const PenaltyManage: React.FC = () => {
     };
 
     const clearPenalties = () => {
-        setPenalties([{reason: menuItems[0], remark: ''}]);
+        setPenalties([{ reason: menuItems[0], remark: '' }]);
+    };
+
+    const uploadPenalties = () => {
+        const hasEmptyRemark = penalties.some(penalty => penalty.remark === '');
+        if (hasEmptyRemark) {
+            setAlertModal(true);
+        } else {
+            setConfirmModal(true);
+        }
+    };
+
+    const closeAlertModal = () => {
+        setAlertModal(false);
+    };
+
+    const closeConfirmModal = () => {
+        setConfirmModal(false);
     };
 
     return (
@@ -53,8 +73,27 @@ const PenaltyManage: React.FC = () => {
             ))}
             <AddPenaltyText onClick={addPenalty}>페널티 추가하기</AddPenaltyText>
             <ButtonContainer>
-                <Button onClick={clearPenalties} text={"모두 삭제"} />
+                <ButtonWrapper>
+                    <Button onClick={clearPenalties} text={"모두 삭제"} />
+                </ButtonWrapper>
+                <ButtonWrapper>
+                    <Button text="확인" onClick={uploadPenalties} />
+                </ButtonWrapper>
             </ButtonContainer>
+            {alertModal && 
+                <Modal
+                    title='내용을 입력하세요'
+                    description='비고란에 빈 값이 존재합니다.'
+                    onClose={closeAlertModal} 
+                />
+            }
+            {confirmModal && 
+                <Modal
+                    title='됐도이'
+                    description='모든 비고 값이 입력되었습니다.'
+                    onClose={closeConfirmModal} 
+                />
+            }
         </PenaltyContainer>
     );
 };
@@ -92,7 +131,7 @@ const PenaltyInputHeader = styled.div`
 
 const PenaltyRow = styled.div`
     width: 100%;
-    height: 40px;
+    height: 35px;
     display: flex;
     margin: 10px 0px;
     align-items: center;
@@ -120,4 +159,10 @@ const AddPenaltyText = styled.div`
 const ButtonContainer = styled.div`
     margin-top: 20px;
     margin-left: auto;
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const ButtonWrapper = styled.div`
+    margin: 10px;
 `;
