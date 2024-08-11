4292181858435b154cdc6533efd2ea1bf98983dc
HOAN_C <hoan.c9907@gmail.com>
2024-08-12 01:06:25 +0900

    feat : 로그인 API 연동
    - 로그인 API 연결 및 로그인 후 라우팅 기능 구현
    
    로그인을 위한 페이지를 구현 #8

---
+++ src/API/auth.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://moaboa.shop/';
+
+interface LoginResponse {
+    accessToken: string;
+    refreshToken: string;
+    memberName: string;
+    memberRole: string;
+}
+
+const login = async (email: string, password: string): Promise<LoginResponse> => {
+    try {
+        const response = await axios.post<LoginResponse>(
+            `${BASE_URL}/auth/login`,
+            { email, password },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            },
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Login API Error:', error);
+        throw error;
+    }
+};
+
+export default login;
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Login from './pages/auth/LoginPage';
 import UserRoutes from './pages/user/UserRoutes';
 import AdminRoutes from './pages/admin/AdminRoutes';
 
 export default function App() {
-    const [role, setRole] = useState<string | null>('admin');
+    const [role, setRole] = useState<string | null>(sessionStorage.getItem('role'));
 
     const handleLoginSuccess = (role: string) => {
         setRole(role);
+        sessionStorage.setItem('role', role);
     };
+
+    useEffect(() => {
+        const savedRole = sessionStorage.getItem('role');
+        if (savedRole) {
+            setRole(savedRole);
+        }
+    }, []);
 
     return (
         <Router>
             <div>
-                {role ? (
-                    role === 'user' ? (
-                        <AdminRoutes />
-                    ) : (
+                {role ? ( //role이 정의되지 않았으면 로그인 페이지로 이동
+                    role === 'USER' ? ( //role이 USRE 이면 <UserRoutes />, 아니면 <AdminRoutes /> 페이지로 이동
                         <UserRoutes />
+                    ) : (
+                        <AdminRoutes />
                     )
                 ) : (
                     <Login onLoginSuccess={handleLoginSuccess} />
