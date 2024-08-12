126371b829b9ad040d4c083fa72f611e81613c56
정조은 <jjy020121@gmail.com>
2024-08-12 15:06:46 +0900

    feat: 인수 관리 목록 조회 API 연동 (#49)
    - 인수 관리 목록 조회 API 연동

--- src/components/admin/step/model/StepListModel.tsx
+++ src/components/admin/step/model/StepListModel.tsx
@@ -1,29 +1,29 @@
 import api, { setAuthorizationToken } from "../../../../API/AxiosInstance";
 
 // name: 검색 키워드, type: 검색 메뉴, page: 선택된 페이지
-const LotteryDetailListModel = async (name: string, type: string, page: number, applyRoundId: string | undefined) => {
+const LotteryDetailListModel = async (name: string, type: string, page: number) => {
     setAuthorizationToken();
 
     // 프론트에서 사용하는 한글로 된 검색 키워드를 서버용으로 변환
     let typeEng = "ALL";
     switch (type) {
-        case "아이디":
-            typeEng = "ID";
+        case "차량정보":
+            typeEng = "VEHICLE";
             break;
-        case "신청자명":
-            typeEng = "NAME";
+        case "인수자명":
+            typeEng = "MEMBER";
             break;
         default:
             break;
     }
 
     try {
-        const response = await api.get(`${import.meta.env.VITE_SERVER_URL}/admin/lotteries/${applyRoundId}`, {
+        const response = await api.get(`${import.meta.env.VITE_SERVER_URL}/admin/takes/reports`, {
             params: {
               keyword: name,
               searchType: typeEng,
               page: page,
-              size: 10,
+              size: 8,
             },
         });
         
