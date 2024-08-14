63d7ac5afc20e36adf32f010e311b5cc6d771ef8
HOAN_C <hoan.c9907@gmail.com>
2024-08-14 13:48:28 +0900

    feat : MyPage 추첨 참여 내역, 페널티 내역 API 연동
    - 추첨 참여 내역 : participationHistory
    - 페널티 내역 : penaltyHistory
    - 페널티 내역 클릭시 상세 조회 페이지 이동 기능 개발 필요
    
    내 정보 페이지 API 연결 #71

---
+++ src/API/user/participationHistory.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_SERVER_URL;
+
+const getDetail = async () => {
+    const accessToken = sessionStorage.getItem('accessToken');
+
+    try {
+        const response = await axios.get(`${BASE_URL}/lotteries`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('API 요청 실패:', error);
+        throw error;
+    }
+};
+
+export default getDetail;
