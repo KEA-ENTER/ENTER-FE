bc9363b683495e87d15975dca1de4a7b84eff925
HOAN_C <hoan.c9907@gmail.com>
2024-08-16 02:23:15 +0900

    feat : 통계 부분 API 연결
    - API 코드 작성(getStatistic.ts)
    - 데이터 출력 코드 작성
    
    통계 API 연결 #79

--- src/components/user/Statistics/WaitingGraph.tsx
+++ src/components/user/Statistics/WaitingGraph.tsx
@@ -11,47 +11,44 @@ interface BarChartProps {
 }
 
 const WaitingGraph: React.FC<BarChartProps> = ({ data }) => {
-    const formattedData = data.map(item => {
-        const waitingAverageNumber = parseFloat(item.waitingAverageNumber);
-        return {
-          round: `${item.round}회`,
-          waitingAverageNumber: isNaN(waitingAverageNumber) ? 0 : waitingAverageNumber,
-        };
-      });
+    const formattedData = data.map((item) => ({
+        round: `${item.round}회`,
+        waitingAverageNumber: parseFloat(item.waitingAverageNumber), // 문자열을 숫자로 변환
+    }));
 
-  return (
-    <ResponsiveBar
-      data={formattedData}
-      keys={['waitingAverageNumber']}
-      indexBy="round"
-      margin={{ top: 50, right: 50, bottom: 50, left: 60 }}
-      padding={0.3}
-      layout="vertical"
-      colors='#FFD400'
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: '회차',
-        legendPosition: 'middle',
-        legendOffset: 32,
-      }}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: '대기 빠짐 인원',
-        legendPosition: 'middle',
-        legendOffset: -40,
-      }}
-      labelSkipWidth={12}
-      labelSkipHeight={12}
-      labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-      animate={true}
-      motionConfig="gentle"
-      minValue={0}
-    />
-  );
+    return (
+        <ResponsiveBar
+            data={formattedData}
+            keys={['waitingAverageNumber']}
+            indexBy="round"
+            margin={{ top: 50, right: 50, bottom: 50, left: 60 }}
+            padding={0.3}
+            layout="vertical"
+            colors="#FFD400"
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: '회차',
+                legendPosition: 'middle',
+                legendOffset: 32,
+            }}
+            axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: '대기 빠짐 인원',
+                legendPosition: 'middle',
+                legendOffset: -40,
+            }}
+            labelSkipWidth={12}
+            labelSkipHeight={12}
+            labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+            animate={true}
+            motionConfig="gentle"
+            minValue={0}
+        />
+    );
 };
 
 export default WaitingGraph;
--- src/pages/user/Statistics/StatisticsPage.tsx
+++ src/pages/user/Statistics/StatisticsPage.tsx
@@ -2,12 +2,10 @@ import styled from 'styled-components';
 import Title from '../../../components/user/UI/Title';
 import SubTitle from '../../../components/user/UI/SubTitle';
 import { useEffect, useState } from 'react';
-import CompetitionData from '../../../components/user/Statistics/CompetitionData';
-import WaitingData from '../../../components/user/Statistics/WaitingData';
-import PercentageModel from '../../../components/user/Statistics/PercentageModel';
-import useUserStore from '../../../stores/userStore';
 import CompetitionGraph from '../../../components/user/Statistics/CompetitionGraph';
 import WaitingGraph from '../../../components/user/Statistics/WaitingGraph';
+import useUserStore from '../../../stores/userStore';
+import { fetchCompetitionData, fetchWaitingData, fetchPercentage } from '../../../API/user/getStatistic';
 
 interface Competition {
     round: number;
@@ -19,41 +17,19 @@ interface Waiting {
     waitingAverageNumber: string;
 }
 
-// 테스트용 임시 경쟁률
-const data = [
-    { round: 242, competitionRate: "23.33" },
-    { round: 241, competitionRate: "12.3" },
-    { round: 240, competitionRate: "33.3" },
-    { round: 239, competitionRate: "1.1" },
-    { round: 238, competitionRate: "2.34455" }
-  ];
-
 export default function StatisticsPage() {
-    const [compData, setCompData] = useState<Competition[]>([]);  // 진짜 경쟁률
-    const [waitData, setWaitData] = useState<Waiting[]>([]);  // 진짜 대기 수
-    const [percentage, setPercentage] = useState("100");
+    const [compData, setCompData] = useState<Competition[]>([]); // 경쟁률 데이터
+    const [percentage, setPercentage] = useState<string>('100');
+    const [waitData, setWaitData] = useState<Waiting[]>([]); // 대기 수 데이터
 
     const { name } = useUserStore((state) => ({
         name: state.name,
     }));
-    
+
     useEffect(() => {
-        CompetitionData().then(res => {
-            if (res) {
-                setCompData(res);
-            }
-        });
-        WaitingData().then(res => {
-            if (res) {
-                setWaitData(res);
-            }
-        });
-        PercentageModel().then(res => {
-            if (res) {
-                const calculatePercentage = (res.scorePercentile * 100).toFixed(2);
-                setPercentage(calculatePercentage);
-            }
-        })
+        fetchPercentage().then(setPercentage).catch(console.error);
+        fetchCompetitionData().then(setCompData).catch(console.error);
+        fetchWaitingData().then(setWaitData).catch(console.error);
     }, [name]);
 
     return (
@@ -61,8 +37,11 @@ export default function StatisticsPage() {
             <Title title="통계" />
             <SubTitle subTitle={`${name}님은 상위 ${percentage}%예요`} />
             <GraphContainer>
-                {compData ? <CompetitionGraph data={data} /> : <div>데이터가 없습니다.</div> }
-                {waitData ? <WaitingGraph data={waitData} /> : <div>데이터가 없습니다.</div> }
+                <SubTitle subTitle="회차별 경쟁률" />
+
+                {compData.length > 0 ? <CompetitionGraph data={compData} /> : <div>경쟁률 데이터가 없습니다.</div>}
+                <SubTitle subTitle="회차별 취소 수" />
+                {waitData.length > 0 ? <WaitingGraph data={waitData} /> : <div>대기 수 데이터가 없습니다.</div>}
             </GraphContainer>
         </Container>
     );
@@ -74,5 +53,5 @@ const Container = styled.div`
 
 const GraphContainer = styled.div`
     width: 100%;
-    height: 300px;
+    height: 175px;
 `;
